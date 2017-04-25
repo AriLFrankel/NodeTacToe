8c327478aa727b2524e666ccd9b965b49d3678b0
Ari Frankel <ari.l.frankel@gmail.com>
2017-04-25 09:38:25 -0500

    first commit

---
+++ spec.js
@@ -0,0 +1,57 @@
+/* eslint-env mocha */
+const { 
+  checkRowWinner, 
+  checkDiagonalWinner,
+  checkColWinner
+  } = require('./tictactoe')
+
+const assert = require('assert')
+
+describe('Should check for winners', () => {
+  const tieBoard = [null,
+    'O', 'O', 'X',
+    'X', 'X', 'O',
+    'O', 'O', 'X',
+  ]
+
+  it('should find row winner', () => {
+    const board = [null,
+      'X', 'X', 'O',
+      'O', 'O', 'O',
+      '7', 'X', '9',
+    ]
+    assert(checkRowWinner(board) === 'O')
+  })
+
+  it('should not find row winner when there is none', () => {
+    assert(checkRowWinner(tieBoard) === false)
+  })
+
+  it('should find col winner in col 1', () => {
+    const board = [null,
+      'X', 'O', '3',
+      'X', 'O', '6',
+      'X', '8', '9',
+    ]
+    assert(checkColWinner(board) === 'X')
+  })
+
+  it('should not find col winner when there is none', () => {
+    assert(checkColWinner(tieBoard) === false)
+  })
+
+  it('should find diagonal winner along minor diagonal', () => {
+    const board = [null,
+      'X', 'X', 'O',
+      'X', 'O', '6',
+      'O', '8', '9',
+    ]
+    assert(checkDiagonalWinner(board) === 'O')
+  })
+
+  it('should not find diagonal winner when there is none', () => {
+    assert(checkDiagonalWinner(tieBoard) === false)
+  })
+
+
+})
---
+++ tictactoe.js
@@ -0,0 +1,76 @@
+const prompt = require('prompt')
+
+const board = [null,
+  '1', '2', '3',
+  '4', '5', '6',
+  '7', '8', '9',
+]
+
+function render(board){
+  return `${board[1]} | ${board[2]} | ${board[3]} \n` +
+  `${board[4]} | ${board[5]} | ${board[6]} \n` +
+  `${board[7]} | ${board[8]} | ${board[9]}`
+}
+
+function checkColWinner (board) {
+  for(var i = 1; i <= 3; i++){
+    if(board[i] === board[i + 3] && board[i] === board[i + 6]) return board[i]
+  }
+  return false
+}
+
+function checkRowWinner (board) {
+  for(var i = 1; i <= 7; i+= 3){
+    if(board[i] === board[i + 1] && board[i] === board[i + 2]) return board[i]
+  }
+  return false
+}
+
+function checkDiagonalWinner (board) {
+  return board[1] === board[5] && board[1] === board[9] ? board[1]
+    : board[7] === board[5] && board[7] === board[3] ? board[7]
+    : false
+}
+
+function turn(player){
+  const positionQuery = `${player}'s turn. choose a position 1-9`
+  const position = {
+    properties: {
+      [positionQuery]: {
+        pattern: /^[1-9]$/,
+        message: 'Choose a position 1-9',
+        required: true
+      }
+    }
+  }
+  prompt.start()
+  prompt.get([position], (err, result) => {
+    // no writing over squares
+    if(board[result[positionQuery]] === 'X' || board[result[positionQuery]] === 'Y'){
+      turn(player)
+      return
+    }
+    board[result[positionQuery]] = player
+    
+    // render board
+    console.log(render(board))
+    
+    // check for winners
+    if(checkColWinner(board) || checkRowWinner(board) || checkDiagonalWinner(board) ){
+      console.log(`${player} wins!`)
+      return
+    }
+
+    player = player === 'X' ? 'Y' : 'X'
+    turn(player)
+  })
+}
+
+turn('X')
+
+module.exports = {
+  checkDiagonalWinner,
+  checkRowWinner,
+  checkColWinner,
+  turn
+}
